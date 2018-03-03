9423f895809c5cd0ee0cdb62169ada728ca3641e
= <karl9242@gmail.com>
2018-03-03 17:22:11 -0600

    added a not found page and a colby page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import ColbyPage from '@/components/ColbyPage';
+import NotFoundComponent from '@/components/NotFoundComponent';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/colby',
+      name: 'Colby',
+      component: ColbyPage,
+    },
+    {
+      path: '*',
+      component: NotFoundComponent,
     },
   ],
 });
